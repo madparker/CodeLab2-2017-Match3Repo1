93689a30f9837a7651fb21d5ac4be750bdee8535
Matthew Parker <maddparker@gmail.com>
2017-09-15 17:46:17 -0400

    Added Matt Sample files from the end of class

---
+++ Match3/Assets/Students/mp612/scripts/MattMatchManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MattMatchManager : MatchManagerScript {
+
+	public override bool GridHasMatch ()
+	{
+		bool hasMatch = base.GridHasMatch ();
+
+		// I PUT SOME CODE IN HERE TO CHECK FOR VERTICAL MATCHES
+		// hasMatch = WHATEVER THIS SHOULD BE || hasMatch;
+
+		return hasMatch;
+	}
+}
