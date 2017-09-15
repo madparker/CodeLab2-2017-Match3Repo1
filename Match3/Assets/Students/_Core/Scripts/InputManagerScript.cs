e5f7242bdaddbc8d41d805920fa70ad579c2e538
8ude <crbertelsen@gmail.com>
2017-09-15 14:18:43 -0400

    comments from Group 3 (Chao Corey Laurenz) on InputManagerScript and MatchManagerScript

--- Match3/Assets/Students/_Core/Scripts/InputManagerScript.cs
+++ Match3/Assets/Students/_Core/Scripts/InputManagerScript.cs
@@ -14,18 +14,30 @@ public class InputManagerScript : MonoBehaviour {
 
 	public virtual void SelectToken(){
 		if(Input.GetMouseButtonDown(0)){
+
+            //project mouse position from screen pos to world position
 			Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			
+            //checks if there's a collider at mousePos
 			Collider2D collider = Physics2D.OverlapPoint(mousePos);
 
 			if(collider != null){
 				if(selected == null){
+                    //select the thing the mouse is pointed at
 					selected = collider.gameObject;
 				} else {
+                    //if we already have a selection, then get the grid positions of the two objects
+                    //(selected and collider)
+
+                    //position of first object selected
 					Vector2 pos1 = gameManager.GetPositionOfTokenInGrid(selected);
+
+                    //position of second object selected
 					Vector2 pos2 = gameManager.GetPositionOfTokenInGrid(collider.gameObject);
 
+                    //check if these two are length 1 away, then evaluate Token exchange
 					if(Mathf.Abs((pos1.x - pos2.x) + (pos1.y - pos2.y)) == 1){
+                        //setup token exchange will try to swap the two items; if this doesn't make a match, move them back
 						moveManager.SetupTokenExchange(selected, pos1, collider.gameObject, pos2, true);
 					}
 
@@ -36,6 +48,7 @@ public class InputManagerScript : MonoBehaviour {
 
 	}
 
+    //is this being used anywhere???
 	public int CommentFunc(int x, int y){
 		return x - y;
 	}
