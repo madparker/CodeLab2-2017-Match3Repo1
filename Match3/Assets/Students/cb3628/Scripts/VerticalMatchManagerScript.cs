502a3b4d3850febdbbf988b4857eb9b35ff392d7
8ude <crbertelsen@gmail.com>
2017-09-29 00:13:30 -0400

    week 3 scene

--- Match3/Assets/Students/cb3628/Scripts/VerticalMatchManagerScript.cs
+++ Match3/Assets/Students/cb3628/Scripts/VerticalMatchManagerScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VerticalMatchManagerScript : CoreyMatchManagerScript {
+public class VerticalMatchManagerScript : MatchManagerScript {
 
 	// Use this for initialization
 
